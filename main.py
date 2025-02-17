8e792d47a402837e79d92b03efce627f567ccc99
Alan <alex297168377@gmai.com>
2025-02-17 18:40:13 +0800

    更新端口配置为9009

--- main.py
+++ main.py
@@ -21,4 +21,4 @@ def index():
 if __name__ == '__main__':
     with app.app_context():
         db.create_all()
-    app.run(host='0.0.0.0', port=3000, debug=True) 
+    app.run(host=app.config['HOST'], port=app.config['PORT'], debug=app.config['DEBUG']) 
