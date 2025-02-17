d0c35b281d105cb38f2e6d292d79730d2eebe6e0
Alan <alex297168377@gmai.com>
2025-02-17 20:58:23 +0800

    fix: 修改部署脚本使用docker执行git

--- deploy.sh
+++ deploy.sh
@@ -1,8 +1,11 @@
 #!/bin/bash
 cd /volume1/docker/stock-app
 
-# 直接使用git pull
-git pull
+# 使用docker执行git pull
+/usr/local/bin/docker run --rm \
+  -v /volume1/docker/stock-app:/app \
+  -w /app \
+  alpine/git pull http://192.168.0.109:3000/alan/stock.git
 
 # 重新构建和启动服务
 /usr/local/bin/docker-compose up -d --build web
