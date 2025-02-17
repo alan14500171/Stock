c71b0d294345be197ed699cc9ee5fcf0da4f2e73
Alan <alex297168377@gmai.com>
2025-02-17 20:43:05 +0800

    保存修改

--- deploy.sh
+++ deploy.sh
@@ -1,8 +1,13 @@
 #!/bin/bash
 cd /volume1/docker/stock-app
 
-# 使用docker容器执行git pull
-sudo docker run --rm -v /volume1/docker/stock-app:/app -w /app alpine/git pull
+# 使用docker容器执行git pull，并传入git凭证
+sudo docker run --rm \
+  -v /volume1/docker/stock-app:/app \
+  -w /app \
+  -e GIT_USERNAME=alan \
+  -e GIT_PASSWORD=$GIT_PASSWORD \
+  alpine/git pull http://alan:${GIT_PASSWORD}@192.168.0.109:3000/alan/stock.git
 
 # 重新构建和启动服务
 sudo /usr/local/bin/docker-compose up -d --build web
